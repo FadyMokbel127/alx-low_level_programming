5e6ab1ced44768921295dbe185ec7e4e91c983d0
Fady Mokbel Edward <fadymokbel2002@gmail.com>
2023-03-16 03:29:14 -0700

    auto-commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* Defining main function that returns 0*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
